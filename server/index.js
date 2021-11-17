07fd6d86814f4b7c4828c0328090b6a6a266741a
Daravy DeRouen <daravyderouen@gmail.com>
2021-11-17 15:05:30 -0600

    Dragged html and css files out of public folder and into main folder

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express()
 
 app.get("/", (req, res) => {   
-    res.sendFile(path.join(__dirname, '../public/store.html'));
+    res.sendFile(path.join(__dirname, '../store.html'));
 
 }) 
 
