7ef91699c8a3d90cc181a67a69e2134c9410823e
Daravy DeRouen <daravyderouen@gmail.com>
2021-11-17 14:52:48 -0600

    push

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const path = require('path');
+
+const app = express()
+
+app.get("/", (req, res) => {   
+    res.sendFile(path.join(__dirname, '..\public\store.html'));
+
+}) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 4005 
+app.listen(port, () => {  
+    console.log(`My app is JAMMIN on port number ${port}`)
+})
